b18558bf9a50dc9900220aa2d885621929f9ec99
Vinal Pawar <vinalpawar1@gmail.com>
2024-12-25 18:14:31 +0530

    added slides

--- week 3/week 3.2/authentication.js
+++ week 3/week 3.2/authentication.js
@@ -1,52 +1,69 @@
 const express = require('express');
 const jwt = require('jsonwebtoken');
-const mongoose = require('mongoose');
 const jwtPassword = '123456';
-const port = process.env.PORT || 3000;
-
-mongoose.connect('');
-
-const User = mongoose.model("User", {
-    name: String,
-    username: String,
-    password: String,
-});
 
 const app = express();
 app.use(express.json());
 
-const userExists = (username, password) => {
+const ALL_USERS = [
+    {
+        username: "harkirat@gmail.com",
+        password: "123",
+        name: "harkirat singh",
+    },
+    {
+        username: "raman@gmail.com",
+        password: "123321",
+        name: "Raman singh",
+    },
+    {
+        username: "priya@gmail.com",
+        password: "123321",
+        name: "Priya kumari",
+    },
+]
 
+const userExists = (username, password) => {
+    // write logic to return true or false if this user exists
+    // in ALL_USERS array
+    for (let i = 0; i < ALL_USERS.length; i++) {
+        if (ALL_USERS[i].username === username && ALL_USERS[i].password === password) {
+            return true;
+        }
+    }
+    return false;
 }
 
-app.post('/signin', (req, res, next) => {
+console.log(userExists('priya@gmail.com', '123321'))
+
+app.post('/signin', (req, res) => {
     const username = req.body.username;
     const password = req.body.password;
 
     if (!userExists(username, password)) {
-        return res.status(403).json({
-            msg: 'User doesnt exist in our in memory db'
+        res.status(403).json({
+            msg: 'User doesnt exist in our in memory db',
         });
     }
 
-    let token = jwt.sign({ username: username }, 'shhhhh');
+    let token = jwt.sign({ username: username }, "shhhhh");
     return res.json({
-        token
+        token,
     });
 });
 
-app.get('/users', (req, res, next) => {
+app.get('/users', (req, res) => {
     const token = req.headers.authorization;
     try {
         const decoded = jwt.verify(token, jwtPassword);
         const username = decoded.username;
+        // return a list of users other than this username
+
     } catch (err) {
         return res.status(403).json({
-            msg: 'Invalid token'
+            msg: 'Invalid Token',
         });
     }
 });
 
-app.listen(port, () => {
-    console.log(`app is listening on port ${port}`);
-});
+// app.listen(3000);
--- week 3/week 3.2/fetch/script.js
+++ week 3/week 3.2/fetch/script.js
@@ -1,4 +1,4 @@
-const getAnimalData = async () => {
+const getUserData = async () => {
     const response = await fetch('https://fakerapi.it/api/v1/persons');
     const finalData = await response.json();
     document.getElementById("container").innerHTML = JSON.stringify(finalData.data);
