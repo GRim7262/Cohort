23013284b394754d1b078992e0b671e58f40604c
Vinal Pawar <vinalpawar1@gmail.com>
2025-02-10 23:04:01 +0530

    frontend

--- week 5/todo-app/backend/index.js
+++ week 5/todo-app/backend/index.js
@@ -1,25 +1,62 @@
 const express = require('express');
+const { createTodo, updateTodo } = require('./types');
+const { todo } = require('./db');
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
-app.post('/todos', (req, res) => {
-    res.send('app.post');
+app.post('/todos', async (req, res) => {
+    const createPayload = req.body;
+    const parsedPayload = createTodo.safeParse(createPayload);
+
+    if (!parsedPayload.success) {
+        res.status(411).json({
+            msg: "You sent the wrong inputs"
+        });
+        return;
+    }
+
+    await todo.create({
+        title: createPayload.title,
+        description: createPayload.description,
+        completed: false,
+    });
+
+    res.status(200).json({
+        msg: "Todo created successfully"
+    });
+
 })
 
-app.get('/todos', (req, res) => {
-    res.send('app.get');
-})
+app.get('/todos', async (req, res) => {
+    const todos = await todo.find({});
 
-app.put('/todos/:id', (req, res) => {
-    res.send("app.put");
-})
+    res.status(200).json({
+        todos
+    });
+});
 
-app.delete('/todos/:id', (req, res) => {
-    res.send("app.delete");
-})
+app.put('/completed/:id', async (req, res) => {
+    const updatePayload = req.body;
+    const parsedPayload = updateTodo.safeParse(updatePayload);
 
+    if (!parsedPayload.success) {
+        res.status(411).json({
+            msg: "You sent the wrong inputs"
+        });
+        return;
+    }
+
+    await todo.update({
+        _id: req.body.id
+    }, {
+        completed: true
+    });
+    res.status(200).json({
+        msg: "Todo marked as completed"
+    })
+});
 
 app.listen(port, () => {
     console.log(`app is listening on ${port}`);
