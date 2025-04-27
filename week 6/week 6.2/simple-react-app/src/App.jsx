4f3c01b981406d860fc1c5cfa335db0cd9143f63
Vinal Pawar <vinalpawar1@gmail.com>
2025-04-27 22:01:44 +0530

    useCallback

--- week 6/week 6.2/simple-react-app/src/App.jsx
+++ week 6/week 6.2/simple-react-app/src/App.jsx
@@ -1,51 +1,26 @@
-import { useMemo, useState } from "react"
-
-// function App() {
-//   const [count, setCount] = useState(0);
-//   const [inputValue, setinputValue] = useState(1);
-
-//   let counter = useMemo(() => {
-//     let finalCount = 0;
-//     for (let i = 1; i <= inputValue; i++) {
-//       finalCount += i;
-//     }
-//     return finalCount;
-//   })
-
-//   return (
-//     <>
-//       <input onChange={(e) => setinputValue(e.target.value)}
-//         placeholder={"Find the sum from 1 to n"} />
-//       <div>Sum from 1 to {inputValue} is {counter}</div>
-//       <button onClick={() => setCount(count + 1)}>Counter {count}</button>
-//     </>
-//   )
-// }
-
-// export default App
+import { memo, useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0);
-  const [inputValue, setinputValue] = useState(1);
+  const [count, setCount] = useState(0)
 
-  let counter = useMemo(() => {
-    let finalCount = 0;
-    for (let i = 1; i <= inputValue; i++) {
-      finalCount += i;
-    }
-    return finalCount;
-  }, [inputValue]);
-  return (
-    <>
-      <input onChange={(e) => {
-        setinputValue(e.target.value);
-      }} />
-      <br /> <br />
-      <div>Sum from 1 to {inputValue} is {counter} </div>
-      <br /> <br />
-      <button onClick={() => setCount(count + 1)}>Count: {count}</button>
-    </>
-  )
+  function onClick() {
+    console.log("child clicked")
+  }
+
+  return <div>
+    <Child onClick={onClick} />
+    <button onClick={() => {
+      setCount(count + 1);
+    }}>Click me {count}</button>
+  </div>
 }
 
-export default App
+const Child = memo(({ onClick }) => {
+  console.log("child render")
+
+  return <div>
+    <button onClick={onClick}>Button clicked</button>
+  </div>
+})
+
+export default App;
