2f19a437aeb1baa881963113ef112ad3ba77c6a4
Vinal Pawar <vinalpawar1@gmail.com>
2025-06-11 22:19:35 +0530

    derived an atom

--- week 7/week 7.4/easy-bits/src/components/ButtonUpdater.jsx
+++ week 7/week 7.4/easy-bits/src/components/ButtonUpdater.jsx
@@ -1,13 +1,14 @@
-import { useSetAtom } from "jotai"
-import { messagingAtom } from "../store/atoms"
+import { useAtomValue, useSetAtom } from "jotai"
+import { messagingAtom, totalNotificationAtom } from "../store/atoms"
 
 
 export default function ButtonUpdater() {
     const setMessagingAtomCount = useSetAtom(messagingAtom);
+    const totalNotificationAtomCount = useAtomValue(totalNotificationAtom);
 
     return (
         <>
-            <button onClick={() => setMessagingAtomCount((prev) => prev + 1)}>Me</button>
+            <button onClick={() => setMessagingAtomCount((prev) => prev + 1)}>Me ({totalNotificationAtomCount})</button>
         </>
     )
 }
--- week 7/week 7.4/easy-bits/src/store/atoms.js
+++ week 7/week 7.4/easy-bits/src/store/atoms.js
@@ -1,9 +1,12 @@
 import { atom } from 'jotai'
 
-export const networkAtom = atom(102);
+export const networkAtom = atom(12);
 
 export const jobsAtom = atom(0);
 
 export const messagingAtom = atom(0);
 
 export const notificationsAtom = atom(12);
+
+export const totalNotificationAtom = atom((get) =>
+    get(networkAtom) + get(jobsAtom) + get(messagingAtom) + get(notificationsAtom))
