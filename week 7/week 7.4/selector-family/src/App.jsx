ce076e373c5a7cd1e77ddf7d1dc7137eb60bbb67
Vinal Pawar <vinalpawar1@gmail.com>
2025-06-19 22:56:39 +0530

    add suspense api

--- week 7/week 7.4/selector-family/src/App.jsx
+++ week 7/week 7.4/selector-family/src/App.jsx
@@ -1,14 +1,17 @@
+import { Suspense } from "react"
 import Todo from "./components/Todo"
 
 function App() {
-
   return (
     <>
+      {/* <Suspense fallback={"Loading..."}> */}
+      <Todo id={1} />
       <Todo id={1} />
       <Todo id={2} />
       <Todo id={3} />
       <Todo id={4} />
       <Todo id={5} />
+      {/* </Suspense> */}
     </>
   )
 }
